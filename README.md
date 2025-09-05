9750e67ecbcb11e17fd60745b4b6e0e5d42db866
Diego Baes <diegobaesv@gmail.com>
2025-09-04 22:15:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pasoapaso_backend
+proyecto spring curso leng prog avanzado sise
---
+++ src/main/java/com/sise/pasoapaso_backend/PasoapasoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sise.pasoapaso_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasoapasoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasoapasoBackendApplication.class, args);
+	}
+
+}
